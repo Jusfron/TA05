3982945fcb240d7fb13775646fdd9a06a908baf3
David Marcos <david.13254@gmail.com>
2022-07-07 09:57:58 +0200

    Tarea 9

---
+++ TA05/src/TA05_9App.java
@@ -0,0 +1,13 @@
+
+public class TA05_9App {
+
+	public static void main(String[] args) {
+		for(int i = 1; i <= 100; i++) {
+			if(i % 3 == 0 || i % 2 == 0){
+				System.out.println(i);
+			}
+		}
+
+	}
+
+}
