d93399101414a40371d66a02159c2349c4103793
David Marcos <david.13254@gmail.com>
2022-07-07 10:21:04 +0200

    arreglar switch tarea 11

--- TA05/src/TA05_11App.java
+++ TA05/src/TA05_11App.java
@@ -8,20 +8,28 @@ public class TA05_11App {
 		switch(dia) {
 		case "lunes":
 			JOptionPane.showMessageDialog(null, dia+" es laborable");
+			break;
 		case "martes":
 			JOptionPane.showMessageDialog(null, dia+" es laborable");
+			break;
 		case "miercoles":
 			JOptionPane.showMessageDialog(null, dia+" es laborable");
+			break;
 		case "jueves":
 			JOptionPane.showMessageDialog(null, dia+" es laborable");
+			break;
 		case "viernes":
 			JOptionPane.showMessageDialog(null, dia+" es laborable");
+			break;
 		case "sabado":
 			JOptionPane.showMessageDialog(null, dia+" es festivo");
+			break;
 		case "domingo":
 			JOptionPane.showMessageDialog(null, dia+" es festivo");
+			break;
 		default:
 			JOptionPane.showMessageDialog(null, dia+" no es un dia de la semana");
+			break;
 		}
 	}
 
