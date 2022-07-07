d91b3a137a509c0157a27555657de7ed7b3dd64f
David Marcos <david.13254@gmail.com>
2022-07-07 09:39:08 +0200

    Tarea 5

--- TA05/src/TA05_5App.java
+++ TA05/src/TA05_5App.java
@@ -1,8 +1,15 @@
+import javax.swing.JOptionPane;
 
 public class TA05_5App {
 
 	public static void main(String[] args) {
-		
+		int num;
+		num = Integer.parseInt(JOptionPane.showInputDialog("Introduce el radio del circulo"));
+		if(num % 2 == 0) {
+			JOptionPane.showMessageDialog(null, num+" es divisible entre 2");
+		} else {
+			JOptionPane.showMessageDialog(null, num+" no es divisible entre 2");
+		}
 
 	}
 
